5073e008366d20ad9235b2074df032125207ac6c
Andrew Szymanek <andrewszymanek@Andrews-MacBook-Pro.local>
2020-01-07 14:42:42 -0600

    gave home hr an id

--- Capstone/Models/Client.cs
+++ Capstone/Models/Client.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
@@ -11,8 +12,14 @@ namespace Capstone.Models
     {
         [Key]
         public int ClientId { get; set; }
+
+        [DisplayName("First Name")]
         public string FirstName { get; set; }
+
+        [DisplayName("Last Name")]
         public string LastName { get; set; }
+
+        [DisplayName("Phone Number")]
         public string PhoneNumber { get; set; }
         public string Email { get; set; }
 
